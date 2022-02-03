309aadda594786104b135aefba588761daac1801
Quentin Chin <q.chin@hotmail.com>
2022-02-03 18:55:07 -0500

    Add footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 // import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 import { Grid, Typography, Card } from "@mui/material";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   const [exercises, setExercises] = useState([]);
@@ -22,76 +23,62 @@ function App() {
       .catch(function (error) {
         console.error(error);
       });
-    //   axios
-    //     .get("https://exercisedb.p.rapidapi.com/exercises/targetList", {
-    //       headers: {
-    //         "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-    //         "x-rapidapi-key": process.env.REACT_APP_EXERCISE_API_KEY,
-    //       },
-    //     })
-    //     .then(function (response) {
-    //       console.log(response.data);
-    //       setTarget(response.data);
-    //     })
-    //     .catch(function (error) {
-    //       console.error(error);
-    //     });
   }, []);
 
-  // the properties of an exercise:
-  // bodyPart, equipment, gifUrl, id, name, & target
-
-  console.log(
-    exercises.filter(
-      (exercise) =>
-        exercise.bodyPart === "back" &&
-        exercise.equipment === "body weight" &&
-        exercise.target === "lats"
-    )
-  );
+  // console.log(
+  //   exercises.filter(
+  //     (exercise) =>
+  //       exercise.bodyPart === "back" &&
+  //       exercise.equipment === "body weight" &&
+  //       exercise.target === "lats"
+  //   )
+  // );
 
   return (
-    <div className="App">
-      <h1>Work Out Workout</h1>
-      <Grid container>
-        <Grid item xs={3}>
-          <Typography>Filter Values</Typography>
-        </Grid>
-        <Grid container xs={9} display={"flex"}>
-          {exercises
-            .filter(
-              (exercise) =>
-                exercise.bodyPart === "back" &&
-                exercise.equipment === "body weight"
-            )
-            .map((item) => (
-              <Card xs={3} style={{ maxWidth: "350px", margin: "1rem" }}>
-                <li
-                  key={item.id}
-                  style={{ listStyle: "none", overflow: "scroll" }}
-                >
-                  <h3
-                    style={{
-                      margin: "0",
-                      display: "flex",
-                      justifyContent: "center",
-                    }}
+    <>
+      <div className="App">
+        <h1>Work Out Workout</h1>
+        <Grid container>
+          <Grid item xs={3}>
+            <Typography>Filter Values</Typography>
+          </Grid>
+          <Grid container xs={9} display={"flex"}>
+            {exercises
+              .filter(
+                (exercise) =>
+                  exercise.bodyPart === "back" &&
+                  exercise.equipment === "body weight"
+              )
+              .map((item) => (
+                <Card xs={3} style={{ maxWidth: "350px", margin: "1rem" }}>
+                  <li
+                    key={item.id}
+                    style={{ listStyle: "none", overflow: "scroll" }}
                   >
-                    {item.name}
-                  </h3>
-                  <div style={{ maxHeight: "420px", maxWidth: "350px" }}>
-                    <img
-                      src={item.gifUrl}
-                      alt="demo"
-                      style={{ width: "100%", height: "100%" }}
-                    />
-                  </div>
-                </li>
-              </Card>
-            ))}
-        </Grid>{" "}
-      </Grid>
-    </div>
+                    <h3
+                      style={{
+                        margin: "0",
+                        display: "flex",
+                        justifyContent: "center",
+                      }}
+                    >
+                      {item.name}
+                    </h3>
+                    <div style={{ maxHeight: "420px", maxWidth: "350px" }}>
+                      <img
+                        src={item.gifUrl}
+                        alt="demo"
+                        style={{ width: "100%", height: "100%" }}
+                      />
+                    </div>
+                  </li>
+                </Card>
+              ))}
+          </Grid>{" "}
+        </Grid>
+      </div>
+      <Footer />
+    </>
   );
 }
 
